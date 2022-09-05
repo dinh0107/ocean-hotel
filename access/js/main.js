97033247584ad318652877fdb5cc376cbd3c027e
dinh0107 <dinhdv01072001@gmail.com>
2022-09-05 15:10:53 +0700

    update

--- access/js/main.js
+++ access/js/main.js
@@ -3,7 +3,7 @@ $('.kindroom-item').slick({
     centerPadding: '60px',
     infinite: true,
     speed: 1500,
-    // autoplay: true,  
+    autoplay: true,  
     autoplaySpeed: 2000,
     variableWidth: true,
     adaptiveHeight: true,
